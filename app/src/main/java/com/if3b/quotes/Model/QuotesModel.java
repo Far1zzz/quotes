919cfa67c9379dbd1998ef3f9b4ef6577f42ec99
farizz <fairizwalker@gmail.com>
2022-11-30 15:10:16 +0700

    step2

---
+++ app/src/main/java/com/if3b/quotes/Model/QuotesModel.java
@@ -0,0 +1,13 @@
+package com.if3b.quotes.Model;
+
+public class QuotesModel {
+    private String quotes, author;
+
+    public String getQuotes() {
+        return quotes;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
